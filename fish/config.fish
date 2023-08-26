d30a1330a9d2810b032119acd0e9bb3666256a7e
Connor Tsui <connor.tsui20@gmail.com>
2023-08-25 23:53:38 -0400

    Fish config cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,12 @@
 clear
 
 # Add git ssh keys
-eval (ssh-agent -c) > /dev/null
+eval (ssh-agent -c) > "/dev/null"
 ssh-add "$HOME/.ssh/git_personal_key"
 ssh-add "$HOME/.ssh/git_cmu_key"
+clear
+
 echo ""
-# clear
 
 if status is-interactive
     neofetch
@@ -25,7 +26,7 @@ abbr -a "python" "python3"
 abbr -a "configs" "cd ~/.config"
 
 # Replace ls with exa
-if command -v exa > /dev/null
+if command -v "exa" > "/dev/null"
     abbr -a "ls" "exa"
     abbr -a "la" "exa -a"
     abbr -a "ll" "exa -al"
@@ -38,12 +39,9 @@ end
 # Type - to move up to top parent dir which is a repository
 function d
     while test $PWD != $HOME
-        if test -d .git
+        if test -d ".git"
             break
         end
         cd ..
     end
 end
-
-
-
