5ed493a557efdb73bb8c71419906c2ea5c06cb11
Connor Tsui <connor.tsui20@gmail.com>
2023-10-02 18:15:34 -0400

    rust stuco abbr

--- fish/config.fish
+++ fish/config.fish
@@ -25,6 +25,7 @@ abbr -a "configs" "cd ~/.config"
 abbr -a "bustub" "cd ~/CMU/CMU-F23/15-445/mybustub"
 abbr -a "451" "cd ~/CMU/CMU-F23/15-451"
 abbr -a "445" "cd ~/CMU/CMU-F23/15-445"
+abbr -a "stuco" "cd ~/CMU/rust-stuco"
 
 # Replace ls with exa
 if command -v "exa" > "/dev/null"
