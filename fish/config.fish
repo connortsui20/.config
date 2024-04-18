04867dcd6f7af221b43f2f3a443784b813fdc26f
Connor Tsui <connor.tsui20@gmail.com>
2024-04-18 13:51:56 -0400

    add starship and fix formatting

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 set fish_greeting ""
 
 # Add git ssh keys
-eval (ssh-agent -c) > "/dev/null"
+eval (ssh-agent -c) >/dev/null
 ssh-add "$HOME/.ssh/git_personal_key"
 ssh-add "$HOME/.ssh/git_cmu_key"
 clear
@@ -14,28 +14,28 @@ if status is-interactive
 end
 
 # Setup all environment variables
-set -gx EDITOR "hx"
+set -gx EDITOR hx
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 # Abbreviations
-abbr -a "pd" "cd .."
-abbr -a "c" "cargo"
-abbr -a "g" "git"
-abbr -a "configs" "cd ~/.config"
-abbr -a "stuco" "cd ~/CMU/rust-stuco"
-abbr -a "afs" "ssh -X cjtsui@linux.andrew.cmu.edu"
-abbr -a "410" "cd ~/CMU/CMU-S24/15-410"
-abbr -a "721" "cd ~/CMU/CMU-S24/15-721/15721-s24-ee1"
+abbr -a pd "cd .."
+abbr -a c cargo
+abbr -a g git
+abbr -a configs "cd ~/.config"
+abbr -a stuco "cd ~/CMU/rust-stuco"
+abbr -a afs "ssh -X cjtsui@linux.andrew.cmu.edu"
+abbr -a 410 "cd ~/CMU/CMU-S24/15-410"
+abbr -a 721 "cd ~/CMU/CMU-S24/15-721/15721-s24-ee1"
 
 # Replace ls with exa
-if command -v "exa" > "/dev/null"
-    abbr -a "ls" "exa"
-    abbr -a "la" "exa -a"
-    abbr -a "ll" "exa -al"
-    abbr -a "lst" "exa --tree"
+if command -v exa >/dev/null
+    abbr -a ls exa
+    abbr -a la "exa -a"
+    abbr -a ll "exa -al"
+    abbr -a lst "exa --tree"
 else
-    abbr -a "la" "ls -a"
-    abbr -a "ll" "ls -al"
+    abbr -a la "ls -a"
+    abbr -a ll "ls -al"
 end
 
 # Type d to move up to top parent dir which is a repository
@@ -47,3 +47,5 @@ function d
         cd ..
     end
 end
+
+starship init fish | source
