435fe18ac3047aa4849df0ad7c8d70b3f9296ae6
Connor Tsui <connor.tsui20@gmail.com>
2024-08-24 16:44:56 -0400

    update for zellij

--- fish/config.fish
+++ fish/config.fish
@@ -54,4 +54,13 @@ function d
     end
 end
 
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 starship init fish | source
